9377dcda712a8b374fb946ed3786e78f4db54618
JizongL <2015rpro@gmail.com>
2020-11-14 00:21:44 -0500

    wrote more tests

--- calculator.go
+++ calculator.go
@@ -1,6 +1,11 @@
 // Package calculator provides a library for simple calculations in Go.
 package calculator
 
+import (
+	"fmt"
+	"math"
+)
+
 // Add takes two numbers and returns the result of adding them together.
 func Add(a, b float64) float64 {
 	return a + b
@@ -13,6 +18,43 @@ func Subtract(a, b float64) float64 {
 }
 
 func Multiply(a, b float64) float64 {
-	
+
 	return a * b
+}
+
+func Divide(a, b float64) (float64, error) {
+	if b == 0 {
+		return 0, fmt.Errorf("bad input: %f, %f (division by zero is undefined)", a, b)
+	}
+	return a / b, nil
+}
+
+func Sqrt(a float64) (float64, error) {
+	if a < 0 {
+		return 0, fmt.Errorf("bad input: %f (square root of negative number is undefined)", a)
+	}
+	return math.Sqrt(a), nil
+}
+
+func AddMany(inputs ...float64) float64 {
+	total := 0.0
+	for _, input := range inputs {
+		total = Add(total, input)
+	}
+	return total
+}
+
+func DivideMany(inputs ...float64) (float64, error) {
+	result := 0.0
+	var err error
+	for _, input := range inputs {
+		if result == 0.0 {
+			result = input
+		}
+		result, err = Divide(result, input)
+		if err != nil {
+			return 0, fmt.Errorf("bad input: %f can't not be 0", input)
+		}
+	}
+	return result, nil
 }
--- calculator_test.go
+++ calculator_test.go
@@ -2,6 +2,10 @@ package calculator_test
 
 import (
 	"calculator"
+	"fmt"
+	"math"
+	"math/rand"
+	"strconv"
 	"testing"
 )
 
@@ -14,6 +18,24 @@ func TestAdd(t *testing.T) {
 	}
 }
 
+func TestAddRandom(t *testing.T) {
+	t.Parallel()
+	r := rand.New(rand.NewSource(99))
+
+	for i := 0; i < 100; i++ {
+		a := r.ExpFloat64()
+		b := r.ExpFloat64()
+		want := a + b
+		name := fmt.Sprintf("TEST adding 2 numbers: %s", strconv.Itoa(i))
+		t.Run(name, func(t *testing.T) {
+			got := calculator.Add(a, b)
+			if want != got {
+				t.Errorf("want %f, got %f", want, got)
+			}
+		})
+	}
+}
+
 func TestSubtract(t *testing.T) {
 	t.Parallel()
 	var want float64 = 2
@@ -23,46 +45,132 @@ func TestSubtract(t *testing.T) {
 	}
 }
 
-func TestMultiply(t *testing.T){
+func TestMultiply(t *testing.T) {
 	t.Parallel()
-	tests:= []struct{
+	tests := []struct {
 		name string
-		a float64
-		b float64
+		a, b float64
 		want float64
 	}{
 		{
-			name:"Test multiplying 2 positive numbers",
-			a:2,
-			b:3,
-			want:6,
+			name: "TEST multiplying 2 numbers",
+			a:    5,
+			b:    2,
+			want: 10,
 		},
 		{
-			name:"Test multiplying 0 and a negative number",
-			a:0,
-			b:-1,
-			want:0,
+			name: "TEST multiplying 0 by a negative number",
+			a:    0,
+			b:    -1,
+			want: 0,
 		},
 		{
-			name:"Test multiplying 2 negative numbers",
-			a:-1,
-			b:-1,
-			want:1,
-		},		
+			name: "TEST multiplying 2 negative numbers",
+			a:    -1,
+			b:    -1,
+			want: 1,
+		},
+		{
+			name: "TEST multiplying 1 by a fractional number",
+			a:    1,
+			b:    0.5,
+			want: 0.5,
+		},
 	}
 
-	// 	got4 := calculator.Multiply(2)
-	// if want2 != got2 && want2 != got4{
-	// 	t.Errorf("want %f, got%f",want2,got2)
-	// }
-	for _,tt:= range tests{
-		t.Run(tt.name,func(t *testing.T){
-			got:=calculator.Multiply(tt.a,tt.b)
-			if got != tt.want{
-				t.Errorf("want %g; got %g",tt.want,got)
-			} 
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := calculator.Multiply(tt.a, tt.b)
+			if tt.want != got {
+				t.Errorf("want %f, got %f", tt.want, got)
+			}
 		})
 	}
+}
 
+func TestDivide(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name        string
+		a           float64
+		b           float64
+		want        float64
+		errExpected bool
+	}{
+		{
+			name:        "TEST dividing 2 numbers",
+			a:           6,
+			b:           2,
+			want:        3,
+			errExpected: false,
+		},
+		{
+			name:        "TEST dividing 6 by 0",
+			a:           6,
+			b:           0,
+			want:        0,
+			errExpected: true,
+		},
+	}
 
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := calculator.Divide(tt.a, tt.b)
+
+			errReceived := err != nil
+			if tt.errExpected != errReceived {
+				t.Fatalf("Divide(%f, %f): unexpected error status: %v", tt.a, tt.b, errReceived)
+			}
+
+			if tt.want != got {
+				t.Errorf("want %f, got %f", tt.want, got)
+			}
+		})
+	}
+}
+
+func TestSqrt(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name        string
+		a           float64
+		want        float64
+		errExpected bool
+	}{
+		{
+			name:        "Test negative input",
+			a:           -1,
+			want:        0,
+			errExpected: true,
+		},
+		{
+			name:        "Test normal input",
+			a:           9,
+			want:        3,
+			errExpected: false,
+		},
+		{
+			name:        "Test decimal input",
+			a:           0.0003,
+			want:        0.01732050807,
+			errExpected: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := calculator.Sqrt(tt.a)
+			errReceived := err != nil
+			if tt.errExpected != errReceived {
+				t.Fatalf("Sqrt(%f): unexpected error status: %v", tt.a, errReceived)
+			}
+
+			if !closeEnough(got, tt.want, 0.000001) {
+				t.Errorf("want %f, got %f", tt.want, got)
+			}
+		})
+	}
+}
+
+func closeEnough(a, b, tolerance float64) bool {
+	return math.Abs(a-b) <= tolerance
 }
